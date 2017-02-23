d0214ec08f3347d07e0a880094a8b095f105427c
Drew Palazzari <drewpalazzari@hotmail.com>
2017-02-23 11:37:49 -0700

    Refactor store service file

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -1,13 +1,15 @@
 class StoreService
 
   def self.find_all(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone&apiKey=#{ENV['API_KEY']}")
-    JSON.parse(response.body, symbolize_names: true)[:stores]
+    JSON.parse(response(zip).body, symbolize_names: true)[:stores]
   end
 
   def self.find_total(zip)
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone&apiKey=#{ENV['API_KEY']}")
-    JSON.parse(response.body, symbolize_names: true)[:total]
+    JSON.parse(response(zip).body, symbolize_names: true)[:total]
+  end
+
+  def self.response(zip)
+    Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone&apiKey=#{ENV['API_KEY']}")
   end
 
 end
